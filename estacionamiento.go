4878df45f4cf75a402f0db08dcdaeef6b1cc4c13
DiegoAlbertoPerezJuarez <213475@ids.upchiapas.edu.mx>
2023-11-18 19:43:11 -0600

    cars get in slots and exit

--- estacionamiento.go
+++ estacionamiento.go
@@ -1,107 +1,572 @@
 package main
 
 import (
-	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2"
-	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/canvas"
-	"fyne.io/fyne/v2/theme"
 	"image/color"
+	"log"
 	"math/rand"
 	"sync"
 	"time"
-	"math"
+
+	"github.com/oakmound/oak/v4"
+	"github.com/oakmound/oak/v4/alg/floatgeom"
+	"github.com/oakmound/oak/v4/entities"
+	"github.com/oakmound/oak/v4/event"
+	"github.com/oakmound/oak/v4/render"
+	"github.com/oakmound/oak/v4/scene"
 )
 
+// Constantes y variables globales
 const (
-	numCajones   = 20
-	maxVehiculos = 100
-	width        = 800
-	height       = 480
+	entranceSpot = 135
+	exitSpot     = 145
+	speed        = 10
 )
-
 var (
-	mutex          sync.Mutex
-	semEntrada     = make(chan bool, 1) // semáforo para la entrada/salida
-	cajones        = make([]bool, numCajones)
-	vehiculos      = make([]*Vehiculo, 0)
-	poissonRate    = 10.0
-	content        = container.NewWithoutLayout()
+	rows       = 4
+	columns    = 5
+	parking    *Parking
+	gateMutex  sync.Mutex
+	carsList   []*Car
+	carsListMutex sync.Mutex
 )
 
-type Vehiculo struct {
-	ID        int
-	Color     color.Color
-	X, Y      float32
-	Rectangle *canvas.Rectangle
-}
-
-func generarVehiculosPoisson() {
-	rate := float64(1) / poissonRate
-	for len(vehiculos) < maxVehiculos {
-		sleepTime := -rate * math.Log(1-rand.Float64())
-		time.Sleep(time.Duration(sleepTime) * time.Second)
-
-		v := &Vehiculo{
-			ID:    len(vehiculos) + 1,
-			Color: color.RGBA{uint8(rand.Intn(255)), uint8(rand.Intn(255)), uint8(rand.Intn(255)), 255},
-		}
-		mutex.Lock()
-		vehiculos = append(vehiculos, v)
-		mutex.Unlock()
-
-		go func(v *Vehiculo) {
-			semEntrada <- true
-			entrarEstacionamiento(v)
-			salirEstacionamiento(v)
-			<-semEntrada
-		}(v)
+// Definiciones de estructuras
+type ParkingSlot struct {
+	area                 *floatgeom.Rect2
+	isAvailable          bool
+	directionsForParking []*struct {
+		Direction string
+		Location  float64
+	}
+	directionsForLeaving []*struct {
+		Direction string
+		Location  float64
 	}
 }
 
-func entrarEstacionamiento(v *Vehiculo) {
-	mutex.Lock()
-	for i := 0; i < numCajones; i++ {
-		if !cajones[i] {
-			cajones[i] = true
-			v.X = float32((i % 10) * 60 + 50)
-			v.Y = float32((i / 10) * 160 + 100)
-			v.Rectangle = canvas.NewRectangle(v.Color)
-			v.Rectangle.Move(fyne.NewPos(float32(float64(v.X)), float32(float64(v.Y))))
-			v.Rectangle.Resize(fyne.NewSize(50, 80))
-			content.Add(v.Rectangle)
-			break
-		}
-	}
-	mutex.Unlock()
-	time.Sleep(time.Duration(rand.Intn(5)+1) * time.Second)
+type Parking struct {
+	slots         []*ParkingSlot
+	queueCars     []Car
+	mutex         sync.Mutex
+	availableCond *sync.Cond
 }
 
-func salirEstacionamiento(v *Vehiculo) {
-	mutex.Lock()
-	for i := 0; i < numCajones; i++ {
-		if cajones[i] && v.X == float32(i%10*60+50) && v.Y == float32(i/10*160+100) {
-			cajones[i] = false
-			v.Rectangle.Hide()
-			break
-		}
-	}
-	mutex.Unlock()
+type Car struct {
+	area     floatgeom.Rect2
+	entity   *entities.Entity
+	mutex    sync.Mutex
+	parkSpot *ParkingSlot
 }
 
 func main() {
-	myApp := app.New()
-	myApp.Settings().SetTheme(theme.DarkTheme())
-	myWindow := myApp.NewWindow("Estacionamiento")
-	myWindow.Resize(fyne.NewSize(width, height))
+	oak.AddScene("main", scene.Scene{
+		Start: mainScene,
+	})
 
-	go generarVehiculosPoisson()
+	oak.Init("main", func(c oak.Config) (oak.Config, error) {
+		c.BatchLoad = true
+		c.Assets.ImagePath = "assets/images"
+		c.Assets.AudioPath = "assets/audio"
+		return c, nil
+	})
+}
 
-	entrada := canvas.NewRectangle(color.RGBA{0x99, 0x00, 0x00, 0xff})
-	entrada.Move(fyne.NewPos(width/2-25, height/2-40))
-	entrada.Resize(fyne.NewSize(50, 80))
-	content.Add(entrada)
+func NewParkingSlot(x, y, x2, y2 float64, column int) *ParkingSlot {
+	directionsForParking := getDirectionForParking(x, y, column)
+	directionsForLeaving := getDirectionsForLeaving()
+	area := floatgeom.NewRect2(x, y, x2, y2)
 
-	myWindow.SetContent(content)
-	myWindow.ShowAndRun()
+	return &ParkingSlot{
+		area:                 &area,
+		isAvailable:          true,
+		directionsForParking: directionsForParking,
+		directionsForLeaving: directionsForLeaving,
+	}
+}
+
+func (p *ParkingSlot) GetArea() *floatgeom.Rect2 {
+	return p.area
+}
+
+func (p *ParkingSlot) GetDirectionsForParking() []*struct {
+	Direction string
+	Location  float64
+} {
+	return p.directionsForParking
+}
+
+func (p *ParkingSlot) GetDirectionsForLeaving() []*struct {
+	Direction string
+	Location  float64
+} {
+	return p.directionsForLeaving
+}
+
+func (p *ParkingSlot) GetIsAvailable() bool {
+	return p.isAvailable
+}
+
+func (p *ParkingSlot) SetIsAvailable(isAvailable bool) {
+	p.isAvailable = isAvailable
+}
+
+func getDirectionForParking(x, y float64, column int) []*struct {
+	Direction string
+	Location  float64
+} {
+	var directions []*struct {
+		Direction string
+		Location  float64
+	}
+
+	leftLocation := map[int]float64{
+		1: 445,
+		2: 355,
+		3: 265,
+		4: 175,
+	}
+
+	if location, ok := leftLocation[column]; ok {
+		directions = append(directions, &struct {
+			Direction string
+			Location  float64
+		}{
+			"left", location,
+		})
+	}
+
+	directions = append(directions, &struct {
+		Direction string
+		Location  float64
+	}{
+		"down", y + 5,
+	})
+	directions = append(directions, &struct {
+		Direction string
+		Location  float64
+	}{
+		"left", x + 5,
+	})
+
+	return directions
+}
+
+func getDirectionsForLeaving() []*struct {
+	Direction string
+	Location  float64
+} {
+	var directions []*struct {
+		Direction string
+		Location  float64
+	}
+
+	directions = append(directions, &struct {
+		Direction string
+		Location  float64
+	}{
+		"down", 425,
+	})
+	directions = append(directions, &struct {
+		Direction string
+		Location  float64
+	}{
+		"right", 475,
+	})
+	directions = append(directions, &struct {
+		Direction string
+		Location  float64
+	}{
+		"up", 135,
+	})
+
+	return directions
+}
+
+func NewParking() *Parking {
+	slots := make([]*ParkingSlot, rows*columns)
+	for i := 0; i < rows; i++ {
+		for j := 0; j < columns; j++ {
+			x := 410 - i*90
+			y := 210 + j*45
+			slots[i*columns+j] = NewParkingSlot(float64(x), float64(y), float64(x+30), float64(y+30), i+1)
+		}
+	}
+
+	p := &Parking{
+		slots: slots,
+	}
+	p.availableCond = sync.NewCond(&p.mutex)
+	return p
+}
+
+func (p *Parking) GetSlots() []*ParkingSlot {
+	return p.slots
+}
+
+func (p *Parking) safeExecute(action func()) {
+	p.mutex.Lock()
+	defer p.mutex.Unlock()
+	action()
+}
+
+func (p *Parking) GetAvailableParkingSlot() *ParkingSlot {
+	var spot *ParkingSlot
+	p.safeExecute(func() {
+		spot = p.findAvailableSpot()
+		if spot != nil {
+			spot.SetIsAvailable(false)
+		}
+	})
+	return spot
+}
+
+func (p *Parking) findAvailableSpot() *ParkingSlot {
+	for _, spot := range p.slots {
+		if spot.GetIsAvailable() {
+			return spot
+		}
+	}
+	p.availableCond.Wait()
+	return p.findAvailableSpot()
+}
+
+func (p *Parking) MakeParkingSlotAvailable(spot *ParkingSlot) {
+	p.safeExecute(func() {
+		spot.SetIsAvailable(true)
+		p.availableCond.Signal()
+	})
+}
+
+func (p *Parking) GetCarsInQueue() (cars []Car) {
+	p.safeExecute(func() {
+		cars = p.queueCars
+	})
+	return cars
+}
+
+func NewCar(ctx *scene.Context) *Car {
+	area := floatgeom.NewRect2WH(445, -20, 32, 32)
+
+	carRender, err := render.LoadSprite("assets/images/car.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	entity := entities.New(
+		ctx,
+		entities.WithRect(area),
+		entities.WithColor(color.RGBA{255, 0, 0, 255}),
+		entities.WithRenderable(carRender),
+		entities.WithDrawLayers([]int{2, 3}),
+	)
+
+	return &Car{
+		area:   area,
+		entity: entity,
+	}
+}
+
+func (c *Car) Move(direction string, target float64, step float64) {
+	for {
+		current := c.getCoordinate(direction)
+		if (step > 0 && current >= target) || (step < 0 && current <= target) {
+			break
+		}
+		if !c.isCollision(direction) {
+			c.shift(direction, step)
+		}
+		time.Sleep(speed * time.Millisecond)
+	}
+}
+
+func (c *Car) shift(direction string, step float64) {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+	if direction == "left" || direction == "right" {
+		c.entity.ShiftX(step)
+	} else {
+		c.entity.ShiftY(step)
+	}
+}
+
+func (c *Car) getCoordinate(direction string) float64 {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+	if direction == "left" || direction == "right" {
+		return c.entity.X()
+	}
+	return c.entity.Y()
+}
+
+func (c *Car) isCollision(direction string) bool {
+	carsListMutex.Lock()
+	defer carsListMutex.Unlock()
+	for _, car := range carsList {
+		if car == c {
+			continue // Skip self
+		}
+		switch direction {
+		case "left":
+			if c.isCollisionLeft(car) {
+				return true
+			}
+		case "right":
+			if c.isCollisionRight(car) {
+				return true
+			}
+		case "up":
+			if c.isCollisionUp(car) {
+				return true
+			}
+		case "down":
+			if c.isCollisionDown(car) {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func (c *Car) isCollisionLeft(car *Car) bool {
+	minDistance := 30.0
+	return c.X() > car.X() && c.X()-car.X() < minDistance && c.Y() == car.Y()
+}
+
+func (c *Car) isCollisionRight(car *Car) bool {
+	minDistance := 30.0
+	return c.X() < car.X() && car.X()-c.X() < minDistance && c.Y() == car.Y()
+}
+
+func (c *Car) isCollisionUp(car *Car) bool {
+	minDistance := 30.0
+	return c.Y() > car.Y() && c.Y()-car.Y() < minDistance && c.X() == car.X()
+}
+
+func (c *Car) isCollisionDown(car *Car) bool {
+	minDistance := 30.0
+	return c.Y() < car.Y() && car.Y()-c.Y() < minDistance && c.X() == car.X()
+}
+
+func safeExecute(action func()) {
+	carsListMutex.Lock()
+	defer carsListMutex.Unlock()
+	action()
+}
+
+func AddCar(car *Car) {
+	safeExecute(func() {
+		carsList = append(carsList, car)
+	})
+}
+
+func RemoveCar(car *Car) {
+	carsListMutex.Lock()
+	for i, c := range carsList {
+		if c == car {
+			carsList = append(carsList[:i], carsList[i+1:]...)
+			break
+		}
+	}
+	carsListMutex.Unlock()
+}
+
+func GetCars() []*Car {
+	carsListMutex.Lock()
+	defer carsListMutex.Unlock()
+	copiedCarsList := make([]*Car, len(carsList))
+	copy(copiedCarsList, carsList)
+	return copiedCarsList
+}
+
+func mainScene(ctx *scene.Context) {
+	parking = NewParking()
+	prepareParking(ctx)
+
+	event.GlobalBind(ctx, event.Enter, func(enterPayload event.EnterPayload) event.Response {
+		for i := 0; i < 100; i++ {
+			go run(ctx)
+			sleepRandomDuration(1000, 2000)
+		}
+		return 0
+	})
+}
+
+func prepareParking(ctx *scene.Context) {
+    // Define el color de fondo sólido
+    backgroundColor := color.RGBA{86, 101, 115, 255} // Gris azulado
+
+    // Asume dimensiones estáticas de la ventana
+    screenWidth := 800  // Reemplaza con el ancho real de la ventana
+    screenHeight := 600 // Reemplaza con la altura real de la ventana
+
+    // Crea un rectángulo de color que cubra toda la pantalla
+    fullScreenRect := render.NewColorBox(screenWidth, screenHeight, backgroundColor)
+
+    // Añade el rectángulo de color al renderizado
+    render.Draw(fullScreenRect, 0) // 0 es la capa base
+
+    // Dibuja las líneas del estacionamiento
+    for _, spot := range parking.GetSlots() {
+        area := spot.GetArea()
+        areaX1 := area.Min.X()
+        areaY1 := area.Min.Y()
+        areaX2 := area.Max.X()
+        areaY2 := area.Max.Y()
+
+        topLine := render.NewLine(areaX1, areaY1, areaX2, areaY1, color.White)
+        render.Draw(topLine, 0)
+
+        leftLine := render.NewLine(areaX1, areaY1, areaX1, areaY2, color.White)
+        render.Draw(leftLine, 0)
+
+        bottomLine := render.NewLine(areaX1, areaY2, areaX2, areaY2, color.White)
+        render.Draw(bottomLine, 0)
+    }
+
+    // Dibuja los límites del estacionamiento
+    // Estos valores deben ser ajustados según tu diseño específico
+    limiteIzquierdo := render.NewLine(100, 125, 100, float64(screenHeight-150), color.White)
+    limiteDerecho := render.NewLine(float64(screenWidth-300), 125, float64(screenWidth-300), float64(screenHeight-150), color.White)
+    limiteSuperior := render.NewLine(100, 125, float64(screenWidth-350), 125, color.White)
+    limiteInferior := render.NewLine(100, float64(screenHeight-150), float64(screenWidth-300), float64(screenHeight-150), color.White)
+
+    render.Draw(limiteIzquierdo, 0)
+    render.Draw(limiteDerecho, 0)
+    render.Draw(limiteSuperior, 0)
+    render.Draw(limiteInferior, 0)
+
+    // Dibuja la entrada del estacionamiento
+    entrada := render.NewLine(100, float64(screenHeight-100), 100, float64(screenHeight), color.White)
+    render.Draw(entrada, 0)
+}
+
+
+func run(ctx *scene.Context) {
+	c := createCar(ctx)
+	parkCar(c)
+	exitCar(c)
+	removeCar(c)
+}
+
+func createCar(ctx *scene.Context) *Car {
+	c := NewCar(ctx)
+	AddCar(c)
+	return c
+}
+
+func parkCar(c *Car) {
+	spotAvailable := parking.GetAvailableParkingSlot()
+	c.Park(spotAvailable)
+	sleepRandomDuration(40000, 50000)
+	c.LeaveSpot()
+	parking.MakeParkingSlotAvailable(spotAvailable)
+}
+
+func exitCar(c *Car) {
+	c.ExitParking()
+	c.GoAway()
+}
+
+func removeCar(c *Car) {
+	RemoveCar(c)
+	c.Remove()
+}
+
+func sleepRandomDuration(min, max int) {
+	randDuration := time.Duration(rand.Intn(max-min+1) + min)
+	time.Sleep(randDuration * time.Millisecond)
+}
+
+func (c *Car) AddToQueue() {
+	c.Move("down", entranceSpot, 1)
+}
+
+func (c *Car) EntryParking() {
+	c.Move("down", entranceSpot, 1)
+}
+
+func (c *Car) ExitParking() {
+	c.Move("up", exitSpot, -1)
+}
+
+func (c *Car) Park(spot *ParkingSlot) {
+	// Asegurarse de que el coche espere su turno para entrar al estacionamiento
+	withMutex(&gateMutex, func() {
+		c.EntryParking()
+	})
+
+	// Mover el coche a su plaza asignada
+	directions := spot.GetDirectionsForParking()
+	for _, direction := range directions {
+		if direction.Direction == "right" || direction.Direction == "down" {
+			c.Move(direction.Direction, direction.Location, 1)
+		} else {
+			c.Move(direction.Direction, direction.Location, -1)
+		}
+	}
+
+	// Estacionar el coche
+	withMutex(&c.mutex, func() {
+		c.parkSpot = spot
+	})
+}
+
+func (c *Car) Leave(spot *ParkingSlot) {
+	directions := spot.GetDirectionsForLeaving()
+	for _, direction := range directions {
+		if direction.Direction == "right" || direction.Direction == "down" {
+			c.Move(direction.Direction, direction.Location, 1)
+		} else {
+			c.Move(direction.Direction, direction.Location, -1)
+		}
+	}
+}
+
+func (c *Car) LeaveSpot() {
+	spotX := c.X()
+	c.Move("left", spotX-30, -1)
+}
+
+func withMutex(m *sync.Mutex, action func()) {
+	m.Lock()
+	defer m.Unlock()
+	action()
+}
+
+func (c *Car) GoAway() {
+	c.Move("up", -20, -1)
+}
+
+func (c *Car) Remove() {
+	c.entity.Destroy()
+}
+
+func (c *Car) safeExecute(action func()) {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+	action()
+}
+
+func (c *Car) X() float64 {
+	var x float64
+	c.safeExecute(func() {
+		x = c.entity.X()
+	})
+	return x
+}
+
+func (c *Car) Y() float64 {
+	var y float64
+	c.safeExecute(func() {
+		y = c.entity.Y()
+	})
+	return y
+}
+
+func (c *Car) SetParkSpot(spot *ParkingSlot) {
+	c.parkSpot = spot
+}
+
+func (c *Car) GetParkSpot() *ParkingSlot {
+	return c.parkSpot
 }
--- go.mod
+++ go.mod
@@ -2,11 +2,17 @@ module concurrentec2
 
 go 1.21.0
 
+require github.com/oakmound/oak/v4 v4.1.0
+
 require (
+	dmitri.shuralyov.com/gpu/mtl v0.0.0-20221208032759-85de2813cf6b // indirect
 	fyne.io/fyne v1.4.3 // indirect
 	fyne.io/fyne/v2 v2.4.1 // indirect
 	fyne.io/systray v1.10.1-0.20230722100817-88df1e0ffa9a // indirect
+	github.com/BurntSushi/xgb v0.0.0-20210121224620-deaf085860bc // indirect
+	github.com/BurntSushi/xgbutil v0.0.0-20190907113008-ad855c713046 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/disintegration/gift v1.2.1 // indirect
 	github.com/ebitengine/purego v0.5.0 // indirect
 	github.com/fredbi/uri v1.0.0 // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
@@ -18,16 +24,23 @@ require (
 	github.com/go-text/render v0.0.0-20230619120952-35bccb6164b8 // indirect
 	github.com/go-text/typesetting v0.0.0-20230905121921-abdbcca6e0eb // indirect
 	github.com/godbus/dbus/v5 v5.1.0 // indirect
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
 	github.com/gopherjs/gopherjs v1.17.2 // indirect
 	github.com/hajimehoshi/ebiten/v2 v2.6.2 // indirect
 	github.com/jezek/xgb v1.1.0 // indirect
+	github.com/jfreymuth/pulse v0.1.0 // indirect
 	github.com/jsummers/gobmp v0.0.0-20151104160322-e2ba15ffa76e // indirect
+	github.com/oakmound/alsa v0.0.2 // indirect
+	github.com/oakmound/libudev v0.2.1 // indirect
+	github.com/oakmound/w32 v2.1.0+incompatible // indirect
+	github.com/oov/directsound-go v0.0.0-20141101201356-e53e59c700bf // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/srwiley/oksvg v0.0.0-20221011165216-be6e8873101c // indirect
 	github.com/srwiley/rasterx v0.0.0-20220730225603-2ab79fcdd4ef // indirect
 	github.com/stretchr/testify v1.8.4 // indirect
 	github.com/tevino/abool v1.2.0 // indirect
 	github.com/yuin/goldmark v1.5.5 // indirect
+	golang.org/x/exp v0.0.0-20220414153411-bcd21879b8fd // indirect
 	golang.org/x/exp/shiny v0.0.0-20230817173708-d852ddb80c63 // indirect
 	golang.org/x/image v0.12.0 // indirect
 	golang.org/x/mobile v0.0.0-20230922142353-e2f452493d57 // indirect
